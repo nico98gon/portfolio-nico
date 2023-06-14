c37071e16ec85094e3899dcc1292b9359a8d5f80
Nico Gonzalez <nicodevelo@gmail.com>
2023-06-14 17:03:35 -0300

    Titles responsive

--- components/Titles.tsx
+++ components/Titles.tsx
@@ -10,19 +10,19 @@ export default function Titles() {
     return (
         <>
             <ul>
-                <li className="my-8 w-[65%] float-left">
+                <li className="my-12 sm:my-8 w-[90%] sm:w-[85%] md:w-[75%] lg:w-[65%] pl-2 sm:p-4 2xl:p-0 sm:float-left">
                     <Link href="/experience" passHref>
-                        <h1 className="flex items-baseline gap-2 text-black font-bold text-5xl"><ArrowIcon />work experience</h1>
+                        <h1 className="flex items-baseline sm:gap-2 text-black font-bold text-4xl sm:text-5xl"><ArrowIcon />work experience</h1>
                     </Link>
-                    <h1 className="text-rose text-lg mt-5 ml-12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut esse tempore dicta doloribus amet non harum, iste neque! Placeat id assumenda maxime magni vel reiciendis rem perferendis hic aut nostrum?</h1>
+                    <h1 className="text-rose text-base sm:text-lg mt-5 sm:ml-12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut esse tempore dicta doloribus amet non harum, iste neque! Placeat id assumenda maxime magni vel reiciendis rem perferendis hic aut nostrum?</h1>
                 </li>
 
                 {pages.sort((a, b) => b.title.localeCompare(a.title)).map((page) => (
-                    <li key={page.slug} className={`my-8 w-[65%] ${page.title === "work mentality" || page.title === "study formation" ? "float-right" : "float-left"}`}>
+                    <li key={page.slug} className={`my-12 sm:my-8 w-[94%] sm:w-[85%] md:w-[75%] lg:w-[65%] pl-2 sm:p-4 2xl:p-0 ${page.title === "work mentality" || page.title === "study formation" ? "sm:float-right" : "sm:float-left"}`}>
                         <Link href={`/${page.slug}`} passHref>
-                            <h1 className="flex items-baseline gap-2 text-black font-bold text-5xl"><ArrowIcon />{page.title}</h1>
+                            <h1 className="flex items-baseline gap-2 text-black font-bold text-4xl sm:text-5xl"><ArrowIcon />{page.title}</h1>
                         </Link>
-                        <h1 className="text-rose text-lg mt-5 ml-12">{page.description}</h1>
+                        <h1 className="text-rose text-lg mt-5 sm:ml-12">{page.description}</h1>
                     </li>
                 ))}
             </ul>
