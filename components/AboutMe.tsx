3a6be2c49f92cc77ecc83979d26537247067a5cd
Nico Gonzalez <nicodevelo@gmail.com>
2023-06-14 17:53:00 -0300

    AboutMe mobile responive

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -39,23 +39,24 @@ export default function AboutMe() {
         <motion.section
             style={{ opacity }}
             ref={targetRef}
-            className="fixed mt-[160px] flex-grow ml-[60%] z-10 right-0"
+            className="fixed mt-[160px] flex-grow sm:ml-[70%] z-10"
         >
             <motion.div
                 style={{ scale, x: "-50%" }}
+                className='w-[340px] ml-[62%] sm:w-full sm:ml-0'
             >
                 <h1 className="font-bold text-3xl font-serif">{name}</h1>
-                <p className="my-5 max-w-[460px] text-neutral-800 dark:text-neutral-200">
+                <p className="my-5 max-w-[720px] sm:max-w-[460px] text-neutral-800 dark:text-neutral-200">
                     {about()}
                 </p>
                 <div className="flex items-start md:items-center my-8 flex-col md:flex-row">
                     <Image
-                    alt={name}
-                    className="rounded-full"
-                    src={avatar}
-                    placeholder="blur"
-                    width={100}
-                    priority
+                        alt={name}
+                        className="rounded-full"
+                        src={avatar}
+                        placeholder="blur"
+                        width={100}
+                        priority
                     />
                     <div className="mt-8 md:mt-0 ml-0 md:ml-6 space-y-2 text-neutral-500 dark:text-neutral-400">
                     <a
@@ -82,7 +83,7 @@ export default function AboutMe() {
                     </Link>
                     </div>
                 </div>
-                <p className="my-5 max-w-[600px] text-neutral-800 dark:text-neutral-200">
+                <p className="my-5 max-w-full sm:max-w-[600px] text-neutral-800 dark:text-neutral-200">
                     {bio()}
                 </p>
                 <ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-500 dark:text-neutral-400">
