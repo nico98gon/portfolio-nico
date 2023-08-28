c2c7bea84f2d9171569a80f088ed8ca89a5def09
Nico Gonzalez <nicodevelo@gmail.com>
2023-08-28 16:40:48 -0300

    MiBotBTC commented

--- components/img/MiBotBTC.tsx
+++ components/img/MiBotBTC.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 
-import BTC from "../../public/img/BTC_30m_1_1.PNG";
+// import BTC from "../../public/img/BTC_30m_1_1.PNG";
 
 
 export default function MiBotBTC() {
     return (
         <>
-            <Image src={BTC} width={900} height={600} alt="MiBot BTC trading graphic" quality={100} />
+            {/* <Image src={BTC} width={900} height={600} alt="MiBot BTC trading graphic" quality={100} /> */}
         </>
     )
 }
